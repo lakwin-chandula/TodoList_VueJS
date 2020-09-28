02bea929422a0aaac3e687af5037e58ca3cb09e8
Lakwin Chandula <lakwinc@opusxenta.com>
2020-09-28 16:32:17 +0530

    Initial Commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input type="text" name="title" placeholder="Add Todo.." v-model="title"/>
+      <input class="btn" type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+        title: ''
+    }
+  },
+  methods:{
+      addTodo(){
+        //   console.log(1);
+        // e.preventDefault();
+        
+        const newTodo = {
+            // id: uuid,
+            title: this.title,
+            completed: false
+        }
+        // send up to parent
+        this.$emit('add-todo', newTodo);
+
+        this.title = '';
+      }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+submit[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the Todo List v1.0.0. It is part of the Vue crash course on YouTube.</p>
+  </div>
+</template>
